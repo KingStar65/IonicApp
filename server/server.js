187105b1c09fbaba6687537f9e84203812b860ed
KingStar65 <66763578+KingStar65@users.noreply.github.com>
2020-06-30 16:42:59 +0800

    make it better

--- server/server.js
+++ server/server.js
@@ -2,7 +2,7 @@ var express  = require('express');
 var app      = express();                               
 var morgan = require('morgan');            
 var bodyParser = require('body-parser');    
-var cors = require('cors');
+var cors = require('cWors');
  
 app.use(morgan('dev'));                                        
 app.use(bodyParser.urlencoded({'extended':'true'}));            
@@ -21,3 +21,7 @@ app.set('port', process.env.PORT || 5000);
 app.listen(app.get('port'), function () {
   console.log('Express server listening on port ' + app.get('port'));
 });
+app.options('*', cors(corsOptions));
+app.get('/', cors(corsOptions),(req, res, next) => {
+res.json({message: 'This route is CORS-enabled for an allowed origin.'});
+})
